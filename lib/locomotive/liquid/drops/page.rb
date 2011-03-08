85955502e11fd081426757d6d00040e6b470bb6a
HPNeo <hpneo@hotmail.com>
2011-03-08 14:32:45 -0500

    Agregado drop para acceder a los contenidos editables de una pagina

--- lib/locomotive/liquid/drops/page.rb
+++ lib/locomotive/liquid/drops/page.rb
@@ -24,10 +24,7 @@ module Locomotive
         end
         
         def editable_elements
-        	puts ::Page.criteria.id(@source.id).class.to_s
-        	puts ::Page.criteria.id(@source.id).first.class.to_s
-        	puts ::Page.criteria.id(@source.id).title
-        	::Page.criteria.id(@source.id).first
+        	::Page.criteria.id(@source.id).first.editable_elements
         end
 
       end
