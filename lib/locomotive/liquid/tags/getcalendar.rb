015364a0994bdffcf994b9c21298ad273b2989b2
HPNeo <hpneo@hotmail.com>
2011-03-09 18:12:05 -0500

    Tag para acceder a calendario

--- lib/locomotive/liquid/tags/getcalendar.rb
+++ lib/locomotive/liquid/tags/getcalendar.rb
@@ -1,12 +1,10 @@
 module Liquid
   module Locomotive
     module Tags
-      class GetCalendar < ::Liquid::Tag
+      class GetCalendar < ::Liquid::Block
 				
 		def initialize(tag_name, markup, tokens, context)
-			
 			super
-			
 			month_name = ::Date.today.strftime "%b"
 			
 			@events = ::ContentType.where(:slug => "events").first.contents.select { |c| c.custom_field_7.strftime("%b")==month_name }
