e45ec9e220730d4cda39eb117764d001ba296804
HPNeo <hpneo@hotmail.com>
2011-03-08 20:18:43 -0500

    Agregado drop para acceder a los elementos editables de una pagina

--- lib/locomotive/liquid/drops/page.rb
+++ lib/locomotive/liquid/drops/page.rb
@@ -24,6 +24,7 @@ module Locomotive
         end
         
         def editable_elements
+        	puts ::Page.criteria.id(@source.id).first.attributes[:editable_elements].class
         	::Page.criteria.id(@source.id).first.attributes[:editable_elements]
         end
 
