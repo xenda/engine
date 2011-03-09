c64cd24a512c6098a937f35bab8c3e61ed451667
HPNeo <hpneo@hotmail.com>
2011-03-08 21:57:21 -0500

    Agregado drop para acceder a los elementos editables de una pagina

--- lib/locomotive/liquid/drops/page.rb
+++ lib/locomotive/liquid/drops/page.rb
@@ -25,8 +25,7 @@ module Locomotive
         
         def editable_elements
         	attributes = ::Page.criteria.id(@source.id).first.attributes['editable_elements']
-        	attributes = attributes.to_h
-        	#attributes = JSON.parse(%Q{attributes})
+        	attributes = JSON.parse(%Q{attributes})
         	puts attributes.inspect
         	#::Page.criteria.id(@source.id).first.attributes[:editable_elements].select{|c| c[:custom_field_4] == @source._slug}
         	#::Page.criteria.id(@source.id).first
