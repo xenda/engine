f53acfb4cfa33e0073acf0abc8e924e70881f4dd
HPNeo <hpneo@hotmail.com>
2011-03-09 18:45:39 -0500

    Tag para acceder a calendario

--- lib/locomotive/liquid/tags/getcalendar.rb
+++ lib/locomotive/liquid/tags/getcalendar.rb
@@ -4,21 +4,20 @@ module Locomotive
       class GetCalendar < ::Liquid::Tag
 		
 		def render(context)
-			puts context.inspect
-			%{
-            	<b>calendario</b>
-          	}
-			#super
-			#month_name = ::Date.today.strftime "%b"
+			month_name = ::Date.today.strftime "%b"
 			
-			#@events = ::ContentType.where(:slug => "events").first.contents.select { |c| c.custom_field_7.strftime("%b")==month_name }
+			@events = ::ContentType.where(:slug => "events").first.contents.select { |c| c.custom_field_7.strftime("%b")==month_name }
 			
-			#render_erb(context, 'shared/calendar', :events => @events)
+			render_erb(context, 'shared/calendar', :events => @events)
 		end
 		
-		#def render_erb(context, file_name, locals = {})
-		#	context.registers[:controller].send(:render_to_string, :partial => file_name, :locals => locals)
-		#end
+		def render_erb(context, file_name, locals = {})
+			if context
+				context.registers[:controller].send(:render_to_string, :partial => file_name, :locals => locals)
+			else
+				@context.registers[:controller].send(:render_to_string, :partial => file_name, :locals => locals)
+			end
+		end
 		
 	end
 	
