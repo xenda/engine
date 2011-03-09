dca7ba2f045e798b91287be59c2de4f9f4e14472
Alvaro Pereyra <alvaro@xendacentral.com>
2011-03-09 18:22:11 -0500

    Testing pursoses only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :write_stuff
+  def write_stuff
+    logger.info Rails.env
+    logger.info Rails.new.to_sym
+    logger.info ENV['S3_KEY_ID']
+    logger.info ENV['S3_SECRET_KEY']
+    logger.info ENV['S3_BUCKET']
+  end
+
   protected
+
+  
 
   # rescue_from Exception, :with => :render_error
   #
