f4c902cc0a2b484918a052aaf2dc879b7c4b7e95
HPNeo <hpneo@hotmail.com>
2011-03-08 23:57:46 -0500

    Agregado drop para acceder a los elementos editables de una pagina

--- lib/locomotive/liquid/tags/getpage.rb
+++ lib/locomotive/liquid/tags/getpage.rb
@@ -4,35 +4,15 @@ module Liquid
       class GetPage < ::Liquid::Block
 
         def initialize(tag_name, markup, tokens, context)
-		    super 
-		    setup_options(markup)
-		    if slug = options[:slug]
-		      @page = ::Page.where(:slug => slug).first
-		    else
-		      @page = Page.where(options[:key] => options[:value]).first
-		    end
+		    super
+		    slug = markup
+		    @page = ::Page.where(:slug => slug).first
 		  end
 		  
 		  def render(context)
-		  	context.scopes.last[:page] = @page
+		  	context.scopes.last['page'] = @page
     		super
-	  	  end
-	  	  
-	  	  private
-
-			  def setup_options(options_list)
-			    options_list = options_list.split(",")
-			    if options_list.size == 1
-			     options[:permalink] = options_list.first.gsub("\"","")
-			    else
-			     options[:key] = options_list.first.gsub("\"","").to_sym
-			     options[:value] = options_list.last.gsub(" ","")
-			    end
-			  end
-			
-			  def options
-			    @options ||= {}
-			  end
+		  end
       end
 
       ::Liquid::Template.register_tag('getpage', GetPage)
