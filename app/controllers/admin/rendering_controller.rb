7f85ca9d302e6e094cd3faa70b3c132dfa63321c
Alvaro Pereyra <alvaro@xendacentral.com>
2011-03-09 18:30:20 -0500

    Testing pursoses only

--- app/controllers/admin/rendering_controller.rb
+++ app/controllers/admin/rendering_controller.rb
@@ -7,6 +7,17 @@ module Admin
 
     before_filter :require_site
 
+    before_filter :write_stuff
+    
+    def write_stuff
+      logger.info Rails.env
+      logger.info Rails.new.to_sym
+      logger.info ENV['S3_KEY_ID']
+      logger.info ENV['S3_SECRET_KEY']
+      logger.info ENV['S3_BUCKET']
+    end
+
+
     def show
       render_locomotive_page
     end
