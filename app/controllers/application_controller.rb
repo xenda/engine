9fab5101b4e4345d4f150fa481650c70ea429008
Alvaro Pereyra <alvaro@xendacentral.com>
2011-03-09 18:43:22 -0500

    Fixes Rails.new misshap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   before_filter :write_stuff
   def write_stuff
     logger.info Rails.env
-    logger.info Rails.new.to_sym
     logger.info ENV['S3_KEY_ID']
     logger.info ENV['S3_SECRET_KEY']
     logger.info ENV['S3_BUCKET']
