b048d6f4c34269c0cdac7c16bc0277a4866b9fc0
HPNeo <hpneo@hotmail.com>
2011-03-09 18:04:31 -0500

    Tag para acceder a calendario

--- lib/locomotive/liquid/tags/getcalendar.rb
+++ lib/locomotive/liquid/tags/getcalendar.rb
@@ -3,33 +3,30 @@ module Liquid
     module Tags
       class GetCalendar < ::Liquid::Tag
 				
-				def initialize(tag_name, markup, tokens, context)
-					
-					super
-					
-					month_name = ::Date.today.strftime "%b"
-					
-					@events = ::ContentType.where(:slug => "events").first.contents.select { |c| c.custom_field_7.strftime("%b")==month_name }
-					
-				end
-				
-				def render(context)
-					puts context.inspect
-					puts "----------------"
-					%{
-						calendar
-					}
-					super
-					#render_erb(context, 'shared/calendar', :events => @events)
-				end
-				
-				def render_erb(context, file_name, locals = {})
-					context.registers[:controller].send(:render_to_string, :partial => file_name, :locals => locals)
-				end
-				
-			end
+		def initialize(tag_name, markup, tokens, context)
 			
-			::Liquid::Template.register_tag('getcalendar', GetCalendar)
+			super
+			
+			month_name = ::Date.today.strftime "%b"
+			
+			@events = ::ContentType.where(:slug => "events").first.contents.select { |c| c.custom_field_7.strftime("%b")==month_name }
+			
+		end
+		
+		def render(context)
+			puts context.inspect
+			puts "----------------"
+			super
+			#render_erb(context, 'shared/calendar', :events => @events)
+		end
+		
+		def render_erb(context, file_name, locals = {})
+			context.registers[:controller].send(:render_to_string, :partial => file_name, :locals => locals)
+		end
+		
+	end
+	
+	::Liquid::Template.register_tag('getcalendar', GetCalendar)
 		end
 	end
 end
