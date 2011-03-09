9176c7ce4a9232b1bcfc537d637cda4a99546eb7
HPNeo <hpneo@hotmail.com>
2011-03-09 18:48:33 -0500

    Tag para acceder a calendario

--- lib/locomotive/liquid/tags/getcalendar.rb
+++ lib/locomotive/liquid/tags/getcalendar.rb
@@ -8,7 +8,7 @@ module Locomotive
 			
 			@events = ::ContentType.where(:slug => "events").first.contents.select { |c| c.custom_field_7.strftime("%b")==month_name }
 			
-			render_erb(context, 'shared/calendar', :events => @events)
+			render_erb(context, '/shared/calendar', :events => @events)
 		end
 		
 		def render_erb(context, file_name, locals = {})
