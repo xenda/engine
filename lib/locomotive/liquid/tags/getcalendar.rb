996ed98dae57073868f683dead77a5213d4aa4fa
HPNeo <hpneo@hotmail.com>
2011-03-09 20:04:17 -0500

    Tag para acceder a calendario

--- lib/locomotive/liquid/tags/getcalendar.rb
+++ lib/locomotive/liquid/tags/getcalendar.rb
@@ -8,28 +8,16 @@ module Locomotive
 			
 			@events = ::ContentType.where(:slug => "events").first.contents.select { |c| c.custom_field_7.strftime("%b")==month_name }
 			
-			template = %{
-			<% calendar_for(events, :year => 2011, :month => 3) do |t| %>
-				<%= t.head('mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun') %>
-				<% t.day(:day_method => start_date) do |day, events| %>
-					<%= day.day %><br />
-					<% events.each do |event| %>
-						<%= h(event.title) %><br />
-					<% end %>
-				<% end %>
-			<% end %>
-			}
-			
-			render_erb(context, template, :events => @events)
+			render_erb(context, "/shared/calendar", :events => @events)
 		end
 		
 		def render_erb(context, file_name, locals = {})
-			::Liquid::Template.parse(file_name).render(locals, :registers=>{:controller => context.registers[:controller]})
-			#if context
-			#	context.registers[:controller].send(:render_to_string, :partial => file_name, :locals => locals)
-			#else
-			#	@context.registers[:controller].send(:render_to_string, :partial => file_name, :locals => locals)
-			#end
+			if context
+				template = context.registers[:controller].send(:render_to_string, :partial => file_name, :locals => locals)
+			else
+				template = @context.registers[:controller].send(:render_to_string, :partial => file_name, :locals => locals)
+			end
+			::Liquid::Template.parse(template).render(locals, :registers=>{:controller => context.registers[:controller]})
 		end
 		
 	end
