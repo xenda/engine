f982d5fcc08e4dfb19a574431707c4933f6c7099
HPNeo <hpneo@hotmail.com>
2011-03-15 15:36:10 -0500

    Actualizando tag getcalendar

--- lib/locomotive/liquid/tags/getcalendar.rb
+++ lib/locomotive/liquid/tags/getcalendar.rb
@@ -7,6 +7,7 @@ module Locomotive
 			month_name = ::Date.today.strftime "%b"
 			
 			@events = ::ContentType.where(:slug => "events").first.contents.select { |c| c.custom_field_7.strftime("%b")==month_name }
+			calendar
 		end
 		
 		def calendar
