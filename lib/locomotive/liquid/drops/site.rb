92c17c8603a42303ca56ab0041a38e5bf9717ebf
HPNeo <hpneo@hotmail.com>
2011-03-16 11:12:22 -0500

    Probando soporte para querystring

--- lib/locomotive/liquid/drops/site.rb
+++ lib/locomotive/liquid/drops/site.rb
@@ -14,7 +14,7 @@ module Locomotive
         end
         
         def params
-        	puts @context.registers[:controller].inspect
+        	puts @context.registers[:controller].class
         	@context.registers[:controller].send(:get_params)
         end
 
