1a9aa677331798f05167bbc06acfbb721fe82aea
HPNeo <hpneo@hotmail.com>
2011-04-05 16:33:40 -0700

    FBPage y FBAlbum

--- app/models/fb_album.rb
+++ app/models/fb_album.rb
@@ -1,20 +1,3 @@
-class FBPage
-	require 'net/http'
-	require 'uri'
-	require 'json'
-
-	attr_accessor :id, :url
-	
-	def initialize(id)
-		self.id = id
-		self.url = "http://graph.facebook.com/#{self.id}"
-	end
-	
-	def albums
-		ActiveSupport::JSON.decode(Net::HTTP.get_response(URI.parse("#{self.url}/albums")).body)['data']
-	end
-end
-
 class FBAlbum
 	require 'net/http'
 	require 'uri'
---
+++ app/models/fb_page.rb
@@ -0,0 +1,16 @@
+class FBPage
+	require 'net/http'
+	require 'uri'
+	require 'json'
+
+	attr_accessor :id, :url
+	
+	def initialize(id)
+		self.id = id
+		self.url = "http://graph.facebook.com/#{self.id}"
+	end
+	
+	def albums
+		ActiveSupport::JSON.decode(Net::HTTP.get_response(URI.parse("#{self.url}/albums")).body)['data']
+	end
+end
