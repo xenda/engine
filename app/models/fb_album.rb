3fe58c4b216c6cc2a5c611e4bfd8c1a047bcecff
HPNeo <hpneo@hotmail.com>
2011-04-05 19:06:31 -0700

    Devolver array de FBAlbum en FBPage.albums

--- app/models/fb_album.rb
+++ app/models/fb_album.rb
@@ -2,7 +2,6 @@ class FBAlbum
 	require 'net/http'
 	require 'uri'
 	require 'json'
-	require 'ostruct'
 	
 	attr_accessor :id, :url, :data
 	
@@ -10,7 +9,6 @@ class FBAlbum
 		self.id = id
 		self.url = "http://graph.facebook.com/#{self.id}"
 		self.data = ActiveSupport::JSON.decode(Net::HTTP.get_response(URI.parse(self.url)).body)
-		OpenStruct.new(self.data)
 	end
 
 	def cover_photo
