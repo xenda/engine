32d367a5fab7845b16dd66ef2b876db74ced86f4
Alvaro Pereyra <alvaro@xendacentral.com>
2011-04-08 11:11:32 -0500

    Updates right message

--- lib/locomotive/liquid/tags/facebook_tags.rb
+++ lib/locomotive/liquid/tags/facebook_tags.rb
@@ -21,21 +21,6 @@ module Locomotive
 
         def render(context)
           
-          	case context.registers[:page].fullpath
-      				when /index/ 
-      				when /events/
-                @options[:og_title] = context.registers[:page].title
-                @options[:og_url] = "http://peru.info/events/#{context.registers[:page].slug}"
-                @options[:description] = context.registers[:page].contents[0..150]                				  
-      				when /agenda/ then "schedule"
-      				when /archivo/ then "archive"
-      				else 
-                @options[:og_title] = context.registers[:page].title
-                @options[:og_url] = "http://peru.info/news/#{context.registers[:page].slug}"
-                @options[:og_image] = context.registers[:page].photo
-                @options[:description] = context.registers[:page].contents[0..150]
-      			end
-
           output = %Q{ <meta property="og:title" content="#{@options[:og_title]}" />
            <meta property="og:type" content="country" />
            <meta property="og:url" content="#{@options[:og_url]}" />
